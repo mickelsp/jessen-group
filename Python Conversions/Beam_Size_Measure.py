f44bc87cda6f7389ed7145deeefb4fd822c0ec54
mickelsp <mickelsp@gmail.com>
2013-09-12 15:29:12 -0700

    Continued Python conversion
    Had to create virtual environment locally to run my file. Still trying
    to get matplotlib working...

--- Python Conversions/Beam_Size_Measure.py
+++ Python Conversions/Beam_Size_Measure.py
@@ -1,12 +1,15 @@
+#!/Users/work/Documents/Github/work_python/bin/python
+
+### Overview
 #This program takes the results of knife edge measurements, which should have the form of 
 #an error function, and turns them into a 1/e^2 beam size.
 #The short cut is to take only the 90% of max power and the 10% of max power positions.
 
 import numpy as np #import numpy functions
-import matplotlib as mpl #import matplotlib functions
+import matplotlib.pyplot as plt #import matplotlib functions
 import csv #import csv functions
 
-###Options
+### Options
 fontsize = 16;
 shortcut1090 = 1; #%1 if you want to use positions of only 90% and 10% maximum power to determine beam size (shortcut for beam profiling)
 
@@ -28,17 +31,17 @@ if shortcut1090 == 1:
 			position10 = position10 + [float(columns[2])];
 	inputfile.close()
 
-	###Convert lists into numeric vectors
+	### Convert lists into numeric vectors
 	measurementposition = np.array(measurementposition); #[cm]
 	position90 = np.array(position90); #[um]
 	position10 = np.array(position10); #[um]
 
-	###Calculate waist size based on Siegman formula (see p. 94 Pascal's Lab Notebook 1)
+	### Calculate waist size based on Siegman formula (see p. 94 Pascal's Lab Notebook 1)
 	waistsize = abs(position90-position10)/1.28; #[um] 1/e^2 beam radius as a function of measurement position along length of beam
     
-	###Plot waist size versus distance from lens
-    #figure(1)
-    #plot(measurementposition(:),waistsize(:),'sr','MarkerFaceColor','r');
+	### Plot waist size versus distance from lens
+	plt.scatter(measurementposition,waistsize,'ro'); #,'sr','MarkerFaceColor','r'
+	plt.show()
     #hold on
     #set(gca,'FontSize',fontsize,'FontWeight','bold');
     #xlabel('Longitudinal Position [cm]','FontSize',fontsize,'FontWeight','bold');
@@ -48,10 +51,11 @@ if shortcut1090 == 1:
     #%legend('Vertical Size','Horizontal Size','Location','best');
     #%title('Beam Profile of 2988 Laser','FontSize',fontsize,'FontWeight','bold');
 
-	#Manually test
+	### Manually test
 	print waistsize
 	print 'status = done'
 
+### Write data to output file
 outputdata = np.column_stack((measurementposition,waistsize)); #[cm mm] data is output 
 
 outputfilepath = directoryname + outputfilename;
